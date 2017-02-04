915f10169a0af9f322c01d52ba6d702826da655c
Thomas <t.j.stein@student.utwente.nl>
2017-02-04 02:50:33 +0100

    Add canvas class stubs

---
+++ src/engine/view/Canvas.java
@@ -0,0 +1,19 @@
+package engine.view;
+
+import engine.view.CanvasPainter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+/**
+ * Created by thomas on 4-2-17.
+ */
+public class Canvas extends JComponent {
+    java.util.List<CanvasPainter> painters;
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+    }
+}
---
+++ src/engine/view/CanvasPainter.java
@@ -0,0 +1,9 @@
+package engine.view;
+
+import javax.swing.*;
+
+/**
+ * Created by thomas on 4-2-17.
+ */
+public class CanvasPainter extends JComponent {
+}
