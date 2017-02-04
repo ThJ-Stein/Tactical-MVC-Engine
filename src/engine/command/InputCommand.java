b7a67638a08d5c7c1311157a592b599b33dc735b
Thomas <t.j.stein@student.utwente.nl>
2017-02-04 14:39:08 +0100

    Add key listener and key map

---
+++ src/engine/command/InputCommand.java
@@ -0,0 +1,40 @@
+package engine.command;
+
+/**
+ * Created by thomas on 4-2-17.
+ */
+public class InputCommand {
+    //TODO: allow user to generate own set of types
+    public enum Type {
+        CONFIRM,
+        CANCEL,
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        START,
+        STRING
+    }
+
+    private Type type;
+
+    private String message;
+
+    public InputCommand(Type type) {
+        this.type = type;
+        this.message = "";
+    }
+
+    public InputCommand(String message) {
+        this.type = Type.STRING;
+        this.message = message;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/engine/view/View.java
+++ src/engine/view/View.java
@@ -1,7 +1,14 @@
 package engine.view;
 
+import engine.command.InputCommand;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.HashMap;
 
 /**
  * Created by thomas on 4-2-17.
@@ -9,7 +16,76 @@ import java.awt.*;
 public class View extends JFrame {
     protected Canvas canvas;
 
+    HashMap<KeyStroke, InputCommand> keyMap;
+
+    {
+        keyMap = new HashMap<>();
+    }
+
     public View(String s) {
         super(s);
+    }
+
+    public void init() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+
+        setFocusable(true);
+        setupListeners();
+    }
+
+    public void mapKey(char key, InputCommand command) {
+        keyMap.put(KeyStroke.getKeyStroke(key), command);
+    }
+
+    public void setupListeners() {
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                requestFocus();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+
+            }
+        });
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                System.out.println(keyEvent);
+                KeyStroke key = KeyStroke.getKeyStroke(keyEvent.getKeyChar());
+                if (keyMap.containsKey(key)) {
+                    System.out.println(keyMap.get(key));
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+
+            }
+        });
     }
 }
