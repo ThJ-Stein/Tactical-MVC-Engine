99304c66de942711c2525dec76dea3b6ad32aa3e
Thomas <t.j.stein@student.utwente.nl>
2017-02-05 18:28:23 +0100

    Add tile functionity

--- src/implementation/model/GridTile.java
+++ src/implementation/model/GridTile.java
@@ -1,7 +1,40 @@
 package implementation.model;
 
+import util.IntPoint;
+
 /**
  * Created by thomas on 4-2-17.
  */
 public class GridTile {
+    private final int x;
+    private final int y;
+    private final double height;
+
+    private BattleUnit unit = null;
+
+    public GridTile(int x, int y, double height) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public double height() {
+        return height;
+    }
+
+    public BattleUnit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(BattleUnit unit) {
+        this.unit = unit;
+    }
 }
