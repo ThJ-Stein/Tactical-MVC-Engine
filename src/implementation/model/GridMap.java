5bb5eccfadf3038486ba6b053d713ed11fb2e8e1
Thomas <t.j.stein@student.utwente.nl>
2017-02-05 18:30:17 +0100

    Add map functionality and factory method

--- src/implementation/model/GridMap.java
+++ src/implementation/model/GridMap.java
@@ -4,5 +4,39 @@ package implementation.model;
  * Created by thomas on 4-2-17.
  */
 public class GridMap {
-    private GridTile[][] tiles;
+    private final GridTile[][] tiles;
+
+    private GridMap(GridTile[][] tiles) {
+        this.tiles = tiles;
+    }
+
+    public int getHeight() {
+        return tiles.length;
+    }
+
+    public int getWidth() {
+        assert tiles.length > 0;
+        return tiles[0].length;
+    }
+
+    public GridTile getTile(int x, int y) {
+        assert getWidth() > x;
+        assert getHeight() > y;
+
+        return tiles[y][x];
+    }
+    }
+
+    //TODO this method is veeeery unfinished
+    public static GridMap createGridMap() {
+        GridTile[][] tiles = new GridTile[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                tiles[j][i] = new GridTile(i, j, 1);
+            }
+        }
+        GridMap map = new GridMap(tiles);
+        return map;
+    }
 }
