d2a866f6a9a83465ae8ac27ec4c9c49e53e30319
Thomas <t.j.stein@student.utwente.nl>
2017-02-05 18:31:40 +0100

    Create Battle

--- src/implementation/model/Battle.java
+++ src/implementation/model/Battle.java
@@ -9,4 +9,32 @@ public class Battle {
     private GridMap map;
 
     private ArrayList<BattleUnit> units;
+
+    private Battle(GridMap map) {
+        this.map = map;
+        units = new ArrayList<>();
+    }
+
+    public void setUnitOnTile(BattleUnit unit, int x, int y) {
+        units.add(unit);
+        map.getTile(x, y).setUnit(unit);
+        unit.setLocation(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Battle{" +
+                "map=" + "\n" + map + "\n" +
+                ", units=" + units +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Battle battle = new Battle(GridMap.createGridMap());
+
+        BattleUnit unit = new BattleUnit(Unit.createUnitWithJob(Job.getJob("MERCHANT")));
+        battle.setUnitOnTile(unit, 2, 5);
+
+        System.out.println(battle);
+    }
 }
