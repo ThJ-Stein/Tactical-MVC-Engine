2acf715ab5bf5cf84bb9e2c5fefcfb7420eca4d0
Thomas <t.j.stein@student.utwente.nl>
2017-02-05 20:20:15 +0100

    Make map data readable from file

--- src/implementation/model/GridMap.java
+++ src/implementation/model/GridMap.java
@@ -1,11 +1,16 @@
 package implementation.model;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by thomas on 4-2-17.
  */
 public class GridMap {
+    public static final String PATH = "data/maps/";
+
     private final GridTile[][] tiles;
 
     private GridMap(GridTile[][] tiles) {
@@ -51,5 +56,49 @@ public class GridMap {
         }
         GridMap map = new GridMap(tiles);
         return map;
+    }
+
+    public static GridMap readGridmap(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(PATH + path));
+
+        List<String[]> lines = new ArrayList<String[]>();
+
+        String line = null;
+        do {
+            line = reader.readLine();
+            if (line != null) {
+                lines.add(line.split(" "));
+            }
+        } while (line != null);
+
+        reader.close();
+
+        GridTile[][] tiles = new GridTile[lines.size()][lines.get(0).length];
+
+        for (int i = 0; i < tiles[0].length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+
+                String data = lines.get(j)[i];
+                GridTile tile = null;
+
+                switch (data) {
+                    default:
+                        tile = new GridTile(i, j, Integer.parseInt(data));
+                        break;
+                }
+
+                tiles[j][i] = tile;
+            }
+        }
+        GridMap map = new GridMap(tiles);
+        return map;
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(readGridmap("map0.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
