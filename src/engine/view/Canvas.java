85c6bee7ccec2fd48c5e7cdb58ff3662616cdac0
Thomas <t.j.stein@student.utwente.nl>
2017-02-06 00:55:20 +0100

    Write javadoc for canvas and canvaspainter

--- src/engine/view/Canvas.java
+++ src/engine/view/Canvas.java
@@ -1,17 +1,29 @@
 package engine.view;
 
-import engine.view.CanvasPainter;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 /**
+ * This JComponent subclass is the canvas that will be repainted on every tick of the GameLoop. When the GameLoop
+ * (or any other object) calls canvas.repaint(), this class will go through its own list of painters, and have
+ * all of them draw their own content on the canvas.
  * Created by thomas on 4-2-17.
  */
 public class Canvas extends JComponent {
-    ArrayList<CanvasPainter> painters = new ArrayList<CanvasPainter>();
 
+    /**
+     * List of painters that will draw upon the board, in order of index.
+     * TODO: Design more elegant way to handle the order of painting
+     */
+    private ArrayList<CanvasPainter> painters = new ArrayList<>();
+
+    /**
+     * This method gets called when repaint() is called by another object (the GameLoop). It sets the screen to
+     * its background color by calling super.paint(), then it calls the paint() method of all painters in the
+     * painter array, by order of index.
+     * @param graphics the graphics object that gets passed to the method when repaint() is called
+     */
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
@@ -21,7 +33,20 @@ public class Canvas extends JComponent {
         }
     }
 
+    /**
+     * Adds a painter to the end of the list.
+     * @param painter the painter to add
+     */
     public void addPainter(CanvasPainter painter) {
         painters.add(painter);
+    }
+
+    /**
+     * Removes a painter from the list of.
+     * @param painter the painter to remove
+     */
+    public void removePainter(CanvasPainter painter) {
+        assert painters.contains(painter);
+        painters.remove(painter);
     }
 }
--- src/engine/view/CanvasPainter.java
+++ src/engine/view/CanvasPainter.java
@@ -1,11 +1,21 @@
 package engine.view;
 
-import javax.swing.*;
 import java.awt.*;
 
 /**
+ * A painter takes an object, and uses its data to draw something on a Graphics2D object.
+ * A canvas has a list of painters. The canvas calls each painter's paint method once per tick.
+ *
+ * The painter does not need to know which canvas it is that calls him. It only needs to paint a visual
+ * representation of the object on the Graphics2D that gets passed in the paint method.
+ *
  * Created by thomas on 4-2-17.
  */
-public abstract class CanvasPainter {
-    public abstract void paint(Graphics2D g);
+public interface CanvasPainter {
+
+    /**
+     * Paints some image on the canvas to which the Graphics2D object belongs.
+     * @param g the graphics object to paint on
+     */
+    void paint(Graphics2D g);
 }
