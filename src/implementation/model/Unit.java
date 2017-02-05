eea1278b7f9ae55238056a5b95eeaf255c66cd63
Thomas <t.j.stein@student.utwente.nl>
2017-02-05 04:38:29 +0100

    Expand unit constructor

--- src/implementation/model/Unit.java
+++ src/implementation/model/Unit.java
@@ -6,13 +6,30 @@ import java.util.ArrayList;
  * Created by thomas on 4-2-17.
  */
 public class Unit {
+    private final String name;
     private ArrayList<Job> jobs;
 
-    private Job primaryJob;
+    private int primaryJob;
 
     private Stats stats;
 
-    public Unit() {
+    public Unit(String name, ArrayList<Job> jobs, Stats stats) {
+        assert name != null && name.length() > 0;
+        assert jobs != null && jobs.size() > 0;
+        assert stats != null && stats.hasCorrectTotal();
+        assert StatConstraints.combineConstraints(jobs).verifyStats(stats);
+
+        this.name = name;
+        this.jobs = jobs;
+        this.primaryJob = 0;
+        this.stats = stats;
+    }
+
+    public Job getPrimaryJob() {
+        assert primaryJob >= 0 && primaryJob < jobs.size();
+
+        return jobs.get(primaryJob);
+    }
 
     }
 }
