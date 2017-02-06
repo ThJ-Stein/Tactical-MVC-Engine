408a7bf199bffc3611d0c929af44bafaa3a0875f
Thomas <t.j.stein@student.utwente.nl>
2017-02-06 10:22:57 +0100

    Add GUI that runs without IntelliJ

---
+++ src/engine/view/gui/EclipseDebugWindow.java
@@ -0,0 +1,29 @@
+package engine.view.gui;
+
+import engine.view.Canvas;
+
+public class EclipseDebugWindow extends ViewFrame {
+	
+	private Canvas canvas = new Canvas();
+	
+	{
+		getRootPane().add(canvas);
+	}
+
+	public EclipseDebugWindow(String windowTitle) {
+		super(windowTitle);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void println(String debugOutput) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Canvas getCanvas() {
+		return canvas;
+	}
+
+}
